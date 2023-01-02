62b83b40a57753d298be87446a9c101ceeff3a40
priyanshusharmaaug03 <priyanshusharmaaug03@gmail.com>
2023-01-03 01:27:56 +0530

    add link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
     setconvo([...convo, userText]);
 
-    const response = await fetch("http://localhost:8000/", {
+    const response = await fetch("https://thebot.up.railway.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
